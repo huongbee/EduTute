e970d5db3f1433623f901ffb16e5b4ab148652f2
Huong <you@example.com>
2018-02-08 21:06:50 +0700

    validator

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from "@angular/forms"
 
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,8 @@ import { routing } from './app.routing';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     routing
   ],
   providers: [],
--- admin/src/app/register/register.component.ts
+++ admin/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  RegisterForm;
   constructor() { }
 
   ngOnInit() {
+    this.RegisterForm = new FormGroup({
+      fullname: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.email),
+      password: new FormControl('', Validators.pattern('(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}')),
+      rePassword: new FormControl('', Validators.required),
+      birthdate: new FormControl('', Validators.pattern('([0-3]{2})-([0-3]{2})-(\d{4})')),
+      address: new FormControl('', Validators.minLength(5)),
+      gender: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.minLength(9))
+    })
   }
+  register() { }
 
 }
