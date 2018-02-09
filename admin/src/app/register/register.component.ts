9573b072cb9225c4806fb65f98ce09d71d0497ea
Huong <you@example.com>
2018-02-09 10:55:16 +0700

    formValidate

--- admin/src/app/register/register.component.ts
+++ admin/src/app/register/register.component.ts
@@ -13,14 +13,19 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
     this.RegisterForm = new FormGroup({
       fullname: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.email),
-      password: new FormControl('', Validators.pattern('(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}')),
-      rePassword: new FormControl('', Validators.required),
-      birthdate: new FormControl('', Validators.pattern('([0-3]{2})-([0-3]{2})-(\d{4})')),
+      email: new FormControl('', Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}')),
+      password: new FormControl('', Validators.pattern('(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{6,}')),
+      rePassword: new FormControl('', Validators.pattern('(.*).{6,}')),
+      birthdate: new FormControl('', Validators.required),
       address: new FormControl('', Validators.minLength(5)),
       gender: new FormControl('', Validators.required),
       phone: new FormControl('', Validators.minLength(9))
     })
+  }
+  get showPasswordNotMatch(): boolean {
+    const passwordInput = this.RegisterForm.get('password');
+    const rePasswordInput = this.RegisterForm.get('rePassword');
+    return rePasswordInput.touched && (rePasswordInput.value !== passwordInput.value);
   }
   register() { }
 
